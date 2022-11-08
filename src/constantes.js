fff3c8dc1c6fc9add9cfd0066ba767df768e2e6f
Makoski Víctor <makoski.ed@gmail.com>
2022-11-08 17:41:48 -0300

    fixed back url

--- src/constantes.js
+++ src/constantes.js
@@ -1,3 +1,3 @@
 export const FRONT_URL = "http://localhost:3000";
-export const BACK_URL =  "https://foodapiback.onrender.com/";
+export const BACK_URL =  "https://foodapiback.onrender.com";
 /* export const BACK_URL =  "http://localhost:3001"; */
