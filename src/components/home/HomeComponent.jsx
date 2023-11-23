2971325427397dff49562791177e0c32e3637c87
MakoskiVictor <makoski.ed@gmail.com>
2023-11-23 17:25:16 -0300

    change return diets

--- src/components/home/HomeComponent.jsx
+++ src/components/home/HomeComponent.jsx
@@ -2,72 +2,79 @@ import React from "react";
 import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchAllRecipes } from "../../redux/actions/index.js";
-import CardComponent from "../card/CardComponent.jsx"; 
+import CardComponent from "../card/CardComponent.jsx";
 import PaginationComponent from "../pagination/PaginationComponent.jsx";
 import FiltersComponent from "../filters/FiltersComponent.jsx";
 import NavBarComponent from "../navBar/NavBarComponent.jsx";
-import "../home/HomeComponent.css"
+import "../home/HomeComponent.css";
 
 export default function HomeComponent() {
+  const allRecipes = useSelector((state) => state.allRecipes);
+  const dispatch = useDispatch();
+  //UN ORDEN PARA LOS FILTROS
+  const [order, setOrder] = useState("");
 
-    const allRecipes = useSelector((state)=> state.allRecipes); 
-    const dispatch = useDispatch();
-    //UN ORDEN PARA LOS FILTROS
-    const [order, setOrder] = useState("");
-    
-    //PARA PAGINADO
-    const [currentPage, setCurrentPage] = useState(1); 
-    const [recipesPerPage, setRecipesPerPage] = useState(12); 
-    const indexOfLastRecipe = currentPage * recipesPerPage; 
-    const indexOfFirstRecipe = indexOfLastRecipe - recipesPerPage; 
-    const currentRecipes = allRecipes.slice(indexOfFirstRecipe, indexOfLastRecipe); 
+  //PARA PAGINADO
+  const [currentPage, setCurrentPage] = useState(1);
+  const [recipesPerPage, setRecipesPerPage] = useState(12);
+  const indexOfLastRecipe = currentPage * recipesPerPage;
+  const indexOfFirstRecipe = indexOfLastRecipe - recipesPerPage;
+  const currentRecipes = allRecipes.slice(
+    indexOfFirstRecipe,
+    indexOfLastRecipe
+  );
 
+  //CONSTANTE QUE AYUDA CON EL RENDERIZADO
+  const paginated = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
 
-    //CONSTANTE QUE AYUDA CON EL RENDERIZADO
-    const paginated = (pageNumber) =>{
-        setCurrentPage(pageNumber);
-    };
+  useEffect(() => {
+    dispatch(fetchAllRecipes());
+  }, [dispatch]);
 
-    useEffect(()=>{
-        dispatch(fetchAllRecipes());
-    }, [dispatch])
+  return (
+    <div className="home">
+      <div>
+        <NavBarComponent allRecipes={allRecipes} />
+      </div>
 
-    return(
-        <div className="home">
-            <div>
-                <NavBarComponent allRecipes={allRecipes}/>
+      <FiltersComponent
+        state={allRecipes}
+        setOrder={setOrder}
+        setCurrentPage={setCurrentPage}
+      />
+      <div>
+        <div className="cards">
+          {currentRecipes.length > 0 && currentRecipes ? (
+            currentRecipes.map((r, i) => {
+              return (
+                <CardComponent
+                  key={i}
+                  id={r.id}
+                  name={r.name}
+                  image={r.image}
+                  diets={r.diets.map((d) => d)}
+                  healthScore={r.healthScore}
+                />
+              );
+            })
+          ) : (
+            <div className="spinnerContainer">
+              <div className="spinner"></div>
+              <p className="SpinnerText">Ups, seems like nothing is here!</p>
             </div>
-            
-            <FiltersComponent state={allRecipes} setOrder={setOrder} setCurrentPage={setCurrentPage} />
-            <div>
-                <div className="cards">
-                    {
-                        currentRecipes.length > 0 && currentRecipes? currentRecipes.map( (r, i) => {
-                            return( 
-                            <CardComponent
-                                key={i}
-                                id={r.id}
-                                name={r.name}
-                                image= {r.image}
-                                diets= {r.diets}
-                                healthScore= {r.healthScore}
-                                />
-                        )})
-                        : <div className="spinnerContainer"><div className="spinner"></div>
-                        <p className="SpinnerText">Ups, seems like nothing is here!</p>
-                        </div> 
-                    }
-                </div>
-
-            </div>
-            <PaginationComponent
-                allRecipes={allRecipes.length} 
-                recipesPerPage={recipesPerPage}
-                paginated={paginated}
-            />
-            <br />
-
-            <div className="espacio"></div>
+          )}
         </div>
-    )
+      </div>
+      <PaginationComponent
+        allRecipes={allRecipes.length}
+        recipesPerPage={recipesPerPage}
+        paginated={paginated}
+      />
+      <br />
+
+      <div className="espacio"></div>
+    </div>
+  );
 }
